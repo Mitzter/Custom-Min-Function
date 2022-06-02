b528b3d76ca1e36539d2db969aae29aa1e5d0585
Loran Abbas <loranabbas@abv.bg>
2022-06-02 18:40:44 +0300

    Add project files.

---
+++ Custom Min Function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Func<List<int>, int> getMinElement = numbers => numbers.Min();
+
+            Console.WriteLine(getMinElement(numbers));
+        }
+    }
+}
